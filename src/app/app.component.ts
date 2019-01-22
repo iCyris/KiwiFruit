4c4d11aab1b01325a245ef1bd7a3b03d5a8d1ce6
iCyris <cyris1015@gmail.com>
2019-01-22 20:02:07 +0800

    v1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {$} from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'kiwi-fruit';
 }
+
+window.onload = function() {
+    const prewrap = <HTMLElement>document.getElementsByClassName('prewrap')[0];
+    prewrap.style.animation = 'fadeout .8s ease 1.8s forwards';
+
+    prewrap.addEventListener('animationend', function(e) {
+        if (e.animationName === 'fadeout') {
+            prewrap.remove();
+        }
+    });
+};
+
+console.log('> Love kiwi!  -- Cyris');
