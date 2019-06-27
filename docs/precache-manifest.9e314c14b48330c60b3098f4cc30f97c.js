2e414c5b5797372afa356f461e7cb0925de3c307
iCyris <cyris1015@gmail.com>
2019-06-27 09:40:19 +0800

    tweaks

---
+++ docs/precache-manifest.9e314c14b48330c60b3098f4cc30f97c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb0acd75b30a5182a7b00fb40c32555",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92ac4645b4629ab5779c",
+    "url": "/static/css/main.e4c330f4.chunk.css"
+  },
+  {
+    "revision": "296e36ccb72ae7e52df6",
+    "url": "/static/js/2.f9422ea3.chunk.js"
+  },
+  {
+    "revision": "92ac4645b4629ab5779c",
+    "url": "/static/js/main.f195cedb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
